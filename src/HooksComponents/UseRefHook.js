7ef594b2a7ad22b6e6e6b6701e51dcc9235606a6
moaabid <moaabid153@gmail.com>
2022-05-08 08:02:44 +0530

    added userefhook

--- src/HooksComponents/UseRefHook.js
+++ src/HooksComponents/UseRefHook.js
@@ -1,7 +1,18 @@
+import { useRef } from 'react'
+
 export const UseRefHook = () => {
+  const inputRef = useRef(null)
+
+  const changeName = () => {
+    inputRef.current.value = ''
+    inputRef.current.focus()
+  }
+
   return (
     <div>
       <h1>Use ref hook</h1>
+      <input type="text" placeholder="Ex.." ref={inputRef}></input>
+      <button onClick={changeName}>Change Name</button>
     </div>
   )
 }
